0c127039463b058e594f742c9240e35804cca523
clemensgg <csc@soundportal.at>
2023-05-01 17:06:10 +0200

    fix ssh check

--- test.sh
+++ test.sh
@@ -83,7 +83,7 @@ function startProviderChain() {
 
   rm gentx2.json gentx3.json
 
-  COLLECTED=$(ssh provider-chain-validator1 -- sudo cat $PROVIDER_HOME/config/genesis.json | grep $VAL_ACCOUNT3)
+  COLLECTED=$(vagrant ssh provider-chain-validator1 -- sudo cat $PROVIDER_HOME/config/genesis.json | grep $VAL_ACCOUNT3)
   if [ -z $COLLECTED ] ; then
     echo "Collecting gentxs on provider-chain-validator1"
     vagrant ssh provider-chain-validator1 -- sudo $PROVIDER_APP --home $PROVIDER_HOME add-genesis-account $VAL_ACCOUNT2 1500000000000icsstake --keyring-backend test
