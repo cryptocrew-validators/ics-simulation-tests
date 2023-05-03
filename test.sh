33b2a5775c847a4b75a7f2b82ce0d5aa350b875f
clemensgg <csc@soundportal.at>
2023-05-03 15:52:14 +0200

    don't fix soft_opt_out_threshold in final genesis

--- test.sh
+++ test.sh
@@ -372,9 +372,9 @@ function prepareConsumerChain() {
   # Finalize consumer-chain genesis
   jq -s '.[0].app_state.ccvconsumer = .[1] | .[0]' raw_genesis.json ccv.json > final_genesis.json
   
-  # FIX: soft_opt_out_threshold gets lost / isn't returned by gaiad
-  jq '.app_state.ccvconsumer.params |= . + {"soft_opt_out_threshold": "0.05"}' final_genesis.json > final_genesis_with_threshold.json
-  mv final_genesis_with_threshold.json final_genesis.json
+  # # FIX: soft_opt_out_threshold gets lost / isn't returned by gaiad
+  # jq '.app_state.ccvconsumer.params |= . + {"soft_opt_out_threshold": "0.05"}' final_genesis.json > final_genesis_with_threshold.json
+  # mv final_genesis_with_threshold.json final_genesis.json
 
   # Distribute consumer-chain genesis
   for i in {1..3} ; do 
