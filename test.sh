d63506ac15c6ce4e98d59b18cf32b243e0e0c95e
clemensgg <csc@soundportal.at>
2023-05-03 18:30:21 +0200

    don't re-add relayer key

--- test.sh
+++ test.sh
@@ -404,7 +404,7 @@ function prepareRelayer() {
   #   hermes_config.toml > config.toml
   
   vagrant ssh provider-chain-validator1 -- "sudo echo $RELAYER_MNEMONIC > .mn"
-  vagrant ssh provider-chain-validator1 -- "sudo $HERMES_BIN --config $HERMES_CONFIG keys add --chain provider-chain --mnemonic-file .mn && sudo $HERMES_BIN --config $HERMES_CONFIG keys add --chain consumer-chain --mnemonic-file .mn"
+  vagrant ssh provider-chain-validator1 -- "sudo $HERMES_BIN --config $HERMES_CONFIG keys add --chain provider-chain --mnemonic-file .mn || true && sudo $HERMES_BIN --config $HERMES_CONFIG keys add --chain consumer-chain --mnemonic-file .mn || true"
 }
 
 function waitForConsumerChain() {
