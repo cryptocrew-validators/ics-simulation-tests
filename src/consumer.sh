029d5cf639a1db296e764c0deeea5e50d74ceb95
clemensgg <csc@soundportal.at>
2023-05-04 19:50:50 +0200

    soft_opt_out_threshold: provider version check

--- src/consumer.sh
+++ src/consumer.sh
@@ -46,9 +46,12 @@ function prepareConsumerChain() {
   # Finalize consumer-chain genesis
   jq -s '.[0].app_state.ccvconsumer = .[1] | .[0]' raw_genesis.json ccv.json > final_genesis.json
   
+  if [[ "$PROVIER_VERSION" == *"v9.0"* ]]; then
   # # FIX: soft_opt_out_threshold gets lost / isn't returned by gaiad
-  jq '.app_state.ccvconsumer.params |= . + {"soft_opt_out_threshold": "0.05"}' final_genesis.json > final_genesis_with_threshold.json
-  mv final_genesis_with_threshold.json final_genesis.json
+    echo "Gaiad version <= v9.0.3: Appending `soft_opt_out_threshold` to final_genesis ccvconsumer.params."
+    jq '.app_state.ccvconsumer.params |= . + {"soft_opt_out_threshold": "0.05"}' final_genesis.json > final_genesis_with_threshold.json
+    mv final_genesis_with_threshold.json final_genesis.json
+  fi
 
   # Distribute consumer-chain genesis
   for i in $(seq 1 $NUM_VALIDATORS); do
