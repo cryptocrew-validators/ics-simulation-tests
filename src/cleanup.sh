4b50c16ba4c179fff5737b5390ccade00e7ce366
clemensgg <csc@soundportal.at>
2023-05-06 16:07:53 +0200

    cp env file, don't move

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -3,10 +3,12 @@ set -e
 # copy all generated files to ./tests
 function copyGeneratedFiles() {
   echo "Success!"
-  echo "Copying generated files to ./tests/* ..."
-  find ./ -maxdepth 1 -type f ! \( -name destroy.sh -o -name README.md -o -name hermes_config.toml -o -name setup.sh -o -name test.sh -o -name Vagrantfile \) -exec mv {} ./tests \;
-  echo "Copying hermes_config.toml to ./tests/* ..."
+  echo "Copying generated files to ./tests/*"
+  find ./ -maxdepth 1 -type f ! \( -name destroy.sh -o -name .env -o -name README.md -o -name hermes_config.toml -o -name setup.sh -o -name test.sh -o -name Vagrantfile \) -exec mv {} ./tests \;
+  echo "Copying hermes_config.toml to ./tests/*"
   cp hermes_config.toml ./tests
+  echo "Copying .env to ./tests/.env"
+  cp .env ./tests
 }
 
 function getLogs() {
@@ -14,7 +16,9 @@ function getLogs() {
   vagrant scp provider-chain-validator1:/var/log/hermes.log ./tests/hermes.log
   for i in $(seq 1 $NUM_VALIDATORS); do
     vagrant scp provider-chain-validator${i}:/var/log/chain.log ./tests/chainlog_provider-chain-validator${i}.log
+    echo "Wrote chainlog_provider-chain-validator${i}.log"
     vagrant scp consumer-chain-validator${i}:/var/log/chain.log ./tests/chainlog_consumer-chain-validator${i}.log
+    echo "Wrote chainlog_consumer-chain-validator${i}.log"
   done
 }
 
