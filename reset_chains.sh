3e8b56ef086222dce38e8d1f911f00ee1c0cb409
clemensgg <csc@soundportal.at>
2024-07-23 10:27:12 +0200

    restore pre-upgrade binary if needed

--- reset_chains.sh
+++ reset_chains.sh
@@ -8,6 +8,10 @@ for i in $(seq 1 $NUM_VALIDATORS); do
   vagrant ssh provider-chain-validator${i} -- "pkill $PROVIDER_APP || true && $PROVIDER_APP tendermint unsafe-reset-all --home $PROVIDER_HOME"
   echo "Resetting $CONSUMER_CHAIN_ID-validator${i}..."
   vagrant ssh consumer-chain-validator${i} -- "pkill $CONSUMER_APP || true && $CONSUMER_APP tendermint unsafe-reset-all --home $CONSUMER_HOME"
+  if vagrant ssh consumer-chain-validator${i} -- test -f /usr/local/bin/oldbin; then
+    echo "Restoring sovereign binary: $CONSUMER_APP $CONSUMER_VERSION"
+    vagrant ssh consumer-chain-validator${i} -- mv /usr/local/bin/oldbin /usr/local/bin/$CONSUMER_APP
+  fi
 done
 
 echo "Cleaning up generated files..."
